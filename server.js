1dfe2de7cb55175a845c405d0408691e7ac2c4db
amazonq1231-afk <amazonq1231@gmail.com>
2026-04-28 11:52:11 +0800

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+// THIS makes https://bpi-58ea.onrender.com work
+app.get('/', (req, res) => {
+    const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+    const time = new Date();
+
+    console.log('--- Visitor ---');
+    console.log('IP:', ip);
+    console.log('Device:', userAgent);
+    console.log('Time:', time);
+    console.log('---------------');
+
+    res.redirect('https://example.com');
+});
+
+// THIS keeps /track working too
 app.get('/track', (req, res) => {
     const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
     const userAgent = req.headers['user-agent'];
